213f6aba729dfcaa8968e9dfbbf9c43df05ddaec
Amrish S <113750525+amrishsekar@users.noreply.github.com>
2024-04-04 13:27:37 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -23,12 +23,20 @@ num = 8
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
 
-# Taking kilometers input from the user
-kilometers = float(input("Enter value in kilometers: "))
 
-# conversion factor
-conv_fac = 0.621371
+# Python program to swap two variables
 
-# calculate miles
-miles = kilometers * conv_fac
-print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
