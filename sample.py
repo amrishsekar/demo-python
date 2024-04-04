50f24d7595b2d58e853cce1d96006c9a0f1480fc
Amrish S <113750525+amrishsekar@users.noreply.github.com>
2024-04-04 12:24:36 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,3 +10,15 @@ sum = num1 + num2
 
 # Display the sum
 print(f"The sum of {num1} and {num2} is {sum}")
+
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
