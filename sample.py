bb92febe97bf50529006639c5f58d10b21f8e465
Amrish S <113750525+amrishsekar@users.noreply.github.com>
2024-04-04 13:20:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -22,3 +22,13 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
